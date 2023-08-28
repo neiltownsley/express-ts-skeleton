b6781abc193aff1a8bd0cb4b1959bc4dd7408491
neil.townsley <neil.townsley@wipro.com>
2023-08-28 12:49:27 +0100

    install eslint and prettier

--- test/integrationTests/averageRandomNumberRoute.test.ts
+++ test/integrationTests/averageRandomNumberRoute.test.ts
@@ -1,12 +1,14 @@
-import {randomAverageNumberPath} from "../../src/configurationConstants";
+import { randomAverageNumberPath } from "../../src/configurationConstants";
 import request from "supertest";
 import app from "../../src/app";
 describe("averageRandomNumberRoute", (): void => {
-    it("When averageRandomNumberRoute is called, and external random number service is called, should return a random number, and average number is returned in response", async (): Promise<void> => {
-        const response: request.Response = await request(app).get(randomAverageNumberPath)
-                expect(response.statusCode).toBe(200);
-                expect(response.body.averageNumber).toBeDefined();
-                expect(response.body.averageNumberFixed).toBeDefined();
-                expect(response.body.randomNumbers.length).toEqual(1);
-    });
+  it("When averageRandomNumberRoute is called, and external random number service is called, should return a random number, and average number is returned in response", async (): Promise<void> => {
+    const response: request.Response = await request(app).get(
+      randomAverageNumberPath,
+    );
+    expect(response.statusCode).toBe(200);
+    expect(response.body.averageNumber).toBeDefined();
+    expect(response.body.averageNumberFixed).toBeDefined();
+    expect(response.body.randomNumbers.length).toEqual(1);
+  });
 });
